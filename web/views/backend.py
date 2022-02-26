4f20e2f0c97c6006760b1188dbdd4c76757f7ac5
ananchevegor <84960978+ananchevegor@users.noreply.github.com>
2022-02-27 02:41:03 +0300

    works all things

--- web/views/backend.py
+++ web/views/backend.py
@@ -6,37 +6,34 @@ from web.database.database import collections, db
 
 async def create_new_plagin(request):
     plagins = {
-        "name": "Philips",
+        "name": "Nokia",
+        "color": "black",
+        "description": "something",
         "price": 20000
     }
     collections.insert_one(plagins)
-    return web.json_response(plagins)
+    return web.Response(text=json.dumps({'status': 'success'}))
 
 async def find_by_name(request):
     name = request.query["name"]
     insert = collections.find({'name': name})
-    lst = list(insert)
-    json_data = json.dumps(lst, indent=2, default=str)
+    json_data = json.dumps(list(insert), default=str, indent=2)
     return web.Response(text=json_data)
 
 async def find_by_color(request):
     color = request.query["color"]
     insert = collections.find({'color': color})
-    json_data = json.dumps(list(insert), default=str)
+    json_data = json.dumps(list(insert), default=str, indent=2)
     return web.Response(text=json_data)
 
 async def find_by_id(request):
     id = request.query["id"]
-    insert = collections.find({'_id': ObjectId(id)})
-    json_data = json.dumps(list(insert), default=str)
+    insert = collections.find({'_id': ObjectId(id)}, {'name': True, 'color': True, '_id': False})
+    json_data = json.dumps(list(insert), default=str, indent=2)
     return web.Response(text=json_data)
 
 async def find_by_id_else(request):
     id = request.query["id"]
     insert = collections.find({'_id': id})
-    lst = list(insert)
-    json_data = json.dumps(lst, default=str)
+    json_data = json.dumps(list(insert), default=str, indent=2)
     return web.Response(text=json_data)
-
-
-
