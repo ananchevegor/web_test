9fe05ae280de00d7f5f5a537617753cff5a9f16f
ananchevegor <84960978+ananchevegor@users.noreply.github.com>
2022-02-27 13:15:09 +0300

    last commit

--- web/views/backend.py
+++ web/views/backend.py
@@ -5,35 +5,50 @@ from web.database.database import collections, db
 
 
 async def create_new_plagin(request):
-    plagins = {
-        "name": "Nokia",
-        "color": "black",
-        "description": "something",
-        "price": 20000
-    }
-    collections.insert_one(plagins)
-    return web.Response(text=json.dumps({'status': 'success'}))
+    try:
+        plagins = {
+            "name": "Nokia",
+            "color": "black",
+            "description": "something",
+            "price": 20000
+        }
+        collections.insert_one(plagins)
+        return web.Response(text=json.dumps({'status': 'success'}))
+    except Exception as e:
+        return web.Response(text=json.dumps({'status': str(e)}))
 
 async def find_by_name(request):
-    name = request.query["name"]
-    insert = collections.find({'name': name})
-    json_data = json.dumps(list(insert), default=str, indent=2)
-    return web.Response(text=json_data)
+    try:
+        name = request.query["name"]
+        insert = collections.find({'name': name})
+        json_data = json.dumps(list(insert), default=str, indent=2)
+        return web.Response(text=json_data)
+    except Exception as e:
+        return web.Response(text=json.dumps({'status': str(e)}))
 
 async def find_by_color(request):
-    color = request.query["color"]
-    insert = collections.find({'color': color})
-    json_data = json.dumps(list(insert), default=str, indent=2)
-    return web.Response(text=json_data)
+    try:
+        color = request.query["color"]
+        insert = collections.find({'color': color})
+        json_data = json.dumps(list(insert), default=str, indent=2)
+        return web.Response(text=json_data)
+    except Exception as e:
+        return web.Response(text=json.dumps({'status': str(e)}))
 
 async def find_by_id(request):
-    id = request.query["id"]
-    insert = collections.find({'_id': ObjectId(id)}, {'name': True, 'color': True, '_id': False})
-    json_data = json.dumps(list(insert), default=str, indent=2)
-    return web.Response(text=json_data)
+    try:
+        id = request.query["id"]
+        insert = collections.find({'_id': ObjectId(id)}, {'name': True, 'color': True, '_id': False})
+        json_data = json.dumps(list(insert), default=str, indent=2)
+        return web.Response(text=json_data)
+    except Exception as e:
+        return web.Response(text=json.dumps({'status': str(e)}))
 
 async def find_by_id_else(request):
-    id = request.query["id"]
-    insert = collections.find({'_id': id})
-    json_data = json.dumps(list(insert), default=str, indent=2)
-    return web.Response(text=json_data)
+    try:
+        id = request.query["id"]
+        insert = collections.find({'_id': id})
+        json_data = json.dumps(list(insert), default=str, indent=2)
+        return web.Response(text=json_data)
+    except Exception as e:
+        return web.Response(text=json.dumps({'status': str(e)}))
